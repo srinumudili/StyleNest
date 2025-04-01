d567a651947a98019f470613ae620328940a3a5a
srinumudili <sekharsrinu200@gmail.com>
2025-04-01 11:21:12 +0530

    Fix CORS issue by allowing deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*", credentials: true }));
 
 //MongoDB Connection
 connectDB();
